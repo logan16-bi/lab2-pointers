07dcd6da84ad27dc6c611c6907910305f02629ee
Logan N. Ramirez Rodriguez <logann.ramirezrodiguez16@ejemplo.com>
2026-08-29 20:19:25 -0400

    Ejercicios de direcciones y pointers basicos

--- direcciones.cpp
+++ direcciones.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    cout << "Lab 2 - Pointers" << endl;
-    cout << "Lab 2 - Pointers" << endl;
+int main()
+{
+	int num = 25;
+	cout << "Direccion de num: " << &num << endl;
 
-    return 0;
+	int *ptr = nullptr;
+	ptr = &num;
+	cout << "ptr apunta a: " << ptr << endl;
+	cout << "Valor apuntado (*ptr): " << *ptr << endl;
+
+	*ptr = 100;
+	cout << "num despues de *ptr = 100: " << num << endl;
+
+	double precio = 19.99;
+	double *pptr = &precio;
+	cout << "\nDireccion de precio: " << &precio << endl;
+	cout << "pptr apunta a: " << pptr << endl;
+	cout << "Valor apuntado (*pptr): " << *pptr << endl;
+
+	int *sinInit;
+
+	return 0;
 }
